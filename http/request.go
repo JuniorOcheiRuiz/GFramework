3c0fd08e9ee7fd57be0421e1dc27724d11dabfcd
Junior Ochei Ruiz <mak.hmc@gmail.com>
2018-11-05 03:01:09 +0100

    Adding bodyParser to Request

--- http/request.go
+++ http/request.go
@@ -1,20 +1,47 @@
 package http
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 )
 
 type Request struct {
 	RequestBase *http.Request
-	Url         *url.URL
+	URL         *url.URL
 	Method      string
+	Headers     http.Header
+	Query       url.Values
+	Body        io.ReadCloser
 }
 
 func NewRequest(requestBase *http.Request) *Request {
 	request := &Request{RequestBase: requestBase}
-	request.Url = requestBase.URL
+	request.URL = requestBase.URL
 	request.Method = requestBase.Method
+	request.Body = requestBase.Body
+	request.Query = requestBase.URL.Query()
+	request.Headers = requestBase.Header
+
+	err := parseBodyRequest(request)
+
+	if err != nil {
+		fmt.Println("Error parsing the body request.")
+	}
 
 	return request
+}
+
+func parseBodyRequest(request *Request) error {
+	contentType := request.Headers.Get("Content-Type")
+	var err error
+
+	switch contentType {
+	case "application/x-www-form-urlencoded":
+		err = request.RequestBase.ParseForm()
+
+	}
+
+	return err
 }
